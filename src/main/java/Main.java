0f9e15e3e9376f3107a8ab745e7948ce47f0a285
camilo09m <c.nanco01@ufromail.cl>
2021-09-22 09:49:05 -0300

    primer commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,24 @@
-public class Main {
+import java.text.Normalizer;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    public static String leerEntrada(){
+        Scanner tecla = new Scanner(System.in);
+        String palabra = tecla.next();
+        return palabra;
+    }
+    public static String limpiarpalabra(String cadena) {
+        String limpio = null;
+        if (cadena != null) {
+            String valor = cadena;
+            valor = valor.toLowerCase();
+            limpio = Normalizer.normalize(valor, Normalizer.Form.NFD);
+            limpio = limpio.replaceAll("[(ASCII 255),(N\u0303)(n\u0303),( )(\u00A1)(\u00BF)(\u00B0)(U\u0308)(u\u0308),(u)]", "");
+            limpio = Normalizer.normalize(limpio, Normalizer.Form.NFC);
+        }
+        return limpio;
+    }
 }
